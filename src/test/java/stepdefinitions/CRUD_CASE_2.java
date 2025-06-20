dd91bead66eff4de914fb1872ef10b23d9bb1762
Your Name <you@example.com>
2025-06-20 22:27:54 +0700

    Read & Update Data

--- src/test/java/stepdefinitions/CRUD_CASE_2.java
+++ src/test/java/stepdefinitions/CRUD_CASE_2.java
@@ -4,9 +4,14 @@ import io.cucumber.java.en.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class CRUD_CASE_2 {
     WebDriver driver;
@@ -78,5 +83,70 @@ public class CRUD_CASE_2 {
         String ExpectedName = "Muhamad";
         Assert.assertEquals("Seharunya Muhamad",ExpectedName,ActualName);
         driver.quit();
+    }
+    @And("I Input Photo Profile")
+    public void iInputPhotoProfile() {
+        WebElement InputProfile = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[1]/div/div[2]/input"));
+        String Photo = "D:\\FILE\\FILE DANDI\\FOTO\\PAS FOTO\\dANDI.jpg";
+        InputProfile.sendKeys(Photo);
+    }
+    @And("I Click Button Down Fill")
+    public void iClickButtonDownFill() {
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[1]/div[2]/div[3]/button/i")).click();
+    }
+    @And("I Fill Field Employee Name")
+    public void iFillFieldEmployeeName() {
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div/input")).sendKeys("Dandi");
+    }
+    @And("I Click Button Search")
+    public void iClickButtonSearch() {
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]")).click();
+    }
+    @Then("I Verify Appears Name = Dandi")
+    public void iVerifyAppearsNameDandi() {
+        pause(3000);
+        WebElement Firstname = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div/div/div/div/div[2]/div[1]/div/div[2]"));
+        String ActualName = Firstname.getText();
+        String ExpectedName = "Muhamad Dandi";
+        Assert.assertEquals("Seharunya Muhamad",ExpectedName,ActualName);
+        driver.quit();
+    }
+    @And("I Fill Field Employee ID")
+    public void iFillFieldEmployeeID() {
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[2]/div/div[2]/input")).sendKeys("0385");
+    }
+    @Then("I Verify Appears ID")
+    public void iVerifyAppearsID() {
+        pause(3000);
+        WebElement Value = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div/div/div/div/div[1]/div[1]/div/div/div[2]"));
+        String ActualValue = Value.getText();
+        String ExpectedValue = "0385";
+        Assert.assertEquals(ExpectedValue,ActualValue);
+    }
+    @And("I click button Edit")
+    public void iClickButtonEdit() {
+        pause(3000);
+        driver.findElement(By.cssSelector(".oxd-icon.bi-pencil-fill")).click();
+    }
+    @And("I Update Lastname")
+    public void iUpdateLastname() {
+        pause(3000);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement lastNameInput = wait.until(ExpectedConditions.elementToBeClickable(By.name("lastName")));
+        lastNameInput.sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        pause(4000);
+        driver.findElement(By.name("lastName")).sendKeys("Setiawan");
+    }
+    @And("I Click Button Save")
+    public void iClickButtonSave() {
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[4]/button")).click();
+    }
+    @Then("I Verify Update LastName")
+    public void iVerifyUpdateLastName() {
+        pause(4000);
+        WebElement value = driver.findElement(By.name("lastName"));
+        String ActualValue = value.getDomProperty("value");
+        String Expected = "Setiawan";
+        Assert.assertEquals(Expected,ActualValue);
     }
 }
--- src/test/resources/features/CRUD_CASE_2.feature
+++ src/test/resources/features/CRUD_CASE_2.feature
@@ -5,8 +5,39 @@ Feature: Create Read Update Delete
     When I Login Website and Click Submit
     And I Click Sub Menu PIM
     And I Click +Add
+    And I Input Photo Profile
     And I Fill Data First Name, Middle Name, & Last name
     And I Click Create Login Details
     And I Fill Username, Password, & Confirm Password
     And I click Bottom Save
     Then I Verify Data Add Employee
+
+    Scenario: Read
+      Given I Open Website "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+      When I Login Website and Click Submit
+      And I Click Sub Menu PIM
+      And I Click Button Down Fill
+      And I Fill Field Employee Name
+      And I Click Button Search
+      Then I Verify Appears Name = Dandi
+
+  Scenario: Read_2
+    Given I Open Website "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    When I Login Website and Click Submit
+    And I Click Sub Menu PIM
+    And I Click Button Down Fill
+    And I Fill Field Employee ID
+    And I Click Button Search
+    Then I Verify Appears ID
+
+  Scenario: Update
+    Given I Open Website "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    When I Login Website and Click Submit
+    And I Click Sub Menu PIM
+    And I Click Button Down Fill
+    And I Fill Field Employee Name
+    And I Click Button Search
+    And I click button Edit
+    And I Update Lastname
+    And I Click Button Save
+    Then I Verify Update LastName
